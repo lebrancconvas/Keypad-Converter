4c279d6f79d300b2f88a00181dd86a572977ec20
lebrancconvas <lebrancconvas@gmail.com>
2024-05-25 16:18:34 +0700

    ✨ feat: add input section and display section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,14 @@
   <header>
     <h1>Keypad Converter</h1>
   </header>
+  <section id="input-section">
+    <label for="input">Enter a number</label>
+    <input type="number" id="input" />
+    <button>Convert</button>
+  </section>
+  <section id="display-section">
+    
+  </section>
 </main>
 
 <style>
